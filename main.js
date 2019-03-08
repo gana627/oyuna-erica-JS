580b491e711e0ecd82debed5487fdbc9231aac2b
gana627 <gana9@yahoo.com>
2019-03-08 23:30:55 +1300

    A

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function pagination() {
   var text =
-    '<div class="center">' +
     '<div class="pagination"> <a href="#">&laquo;</a>' +
     '<a href="https://www.oyuna-erica.com/blog">1</a>' +
     '<a href="https://www.oyuna-erica.com/blog2">2</a>' +
@@ -27,7 +26,7 @@ function pagination() {
     '<a href="https://www.oyuna-erica.com/blog23">23</a>' +
     '<a href="https://www.oyuna-erica.com/blog24">24</a>' +
     '<a href="https://www.oyuna-erica.com/blog25">25</a>' +
-    '<a href="#">&raquo;</a> </div>' +
+    '<a href="#">&raquo;</a> ' +
     "</div>";
 
   document.write(text);
@@ -47,13 +46,13 @@ function navigation() {
     '<a href="https://www.oyuna-erica.com/english-lesson23568318">Free Training</a>' +
     '<a href="https://www.oyuna-erica.com/affiliate_page">Affiliate</a>' +
     '<a href="https://www.oyuna-erica.com/login">Login</a>' +
-    '<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>' +
+    '<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="topnavFunction()">&#9776;</a>' +
     "</div>";
 
   document.write(text1);
 }
 
-function myFunction() {
+function topnavFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -62,7 +61,72 @@ function myFunction() {
   }
 }
 
+function pagination2() {
+  var x = document.URL;
+  num = x.substring(32);
 
+  var current = num,
+    last = 25,
+    delta = 2,
+    left = current - delta,
+    right = current + delta + 1,
+    range = [],
+    rangeWithDots = [],
+    l;
 
+  previous = current - 1;
+  next = current + 1;
 
+  if (current == 1) {
+    previous = 1;
+  } else if (current == last) {
+    next = last;
+  }
+  final =
+    '<div class="center"><div class="pagination">' +
+    '<a href="https://www.oyuna-erica.com/blog' +
+    previous +
+    '">&laquo;</a>';
 
+  for (let i = 1; i <= last; i++) {
+    if (i == 1 || i == last || (i >= left && i < right)) {
+      range.push(i);
+    }
+  }
+
+  for (let i of range) {
+    if (l) {
+      if (i - l === 2) {
+        rangeWithDots.push(l + 1);
+      } else if (i - l !== 1) {
+        rangeWithDots.push("...");
+      }
+    }
+    rangeWithDots.push(i);
+    l = i;
+  }
+
+  for (let i of rangeWithDots) {
+    if (i == current) {
+      final +=
+        '<a class="active" href="https://www.oyuna-erica.com/blog' +
+        i +
+        '">' +
+        i +
+        "</a>";
+    } else if (i == "...") {
+      final += '<a href="#" onclick="return false;">...</a>';
+    } else {
+      final +=
+        '<a href="https://www.oyuna-erica.com/blog' + i + '">' + i + "</a>";
+    }
+  }
+
+  final +=
+    '<a href="https://www.oyuna-erica.com/blog' +
+    next +
+    '">&raquo;</a>' +
+    "</div></div>";
+
+  document.write(final);
+}
