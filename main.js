e90d0c49280ad930d5dc52628d25ab8c4ffde95d
gana627 <gana9@yahoo.com>
2019-09-28 09:35:07 +0800

    new

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ function pagination() {
     '<a href="https://www.oyuna-erica.com/blog30">30</a>'  +
     '<a href="https://www.oyuna-erica.com/blog31">31</a>'  +
     '<a href="https://www.oyuna-erica.com/blog32">32</a>'  +
+    '<a href="https://www.oyuna-erica.com/blog32">33</a>'  +
     '<a href="#">&raquo;</a> ' +
     "</div></div>";
 
