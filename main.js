d6dc1f7cef548b17684ed258e7a8051e8c2cb00f
gana627 <gana9@yahoo.com>
2020-02-10 19:45:14 +0800

    Update main.js

--- main.js
+++ main.js
@@ -37,8 +37,9 @@ function pagination() {
     '<a href="https://www.oyuna-erica.com/blog34">34</a>'  +
     '<a href="https://www.oyuna-erica.com/blog35">35</a>'  +
     '<a href="https://www.oyuna-erica.com/blog36">36</a>'  +
-     '<a href="https://www.oyuna-erica.com/blog37">37</a>'  +
-      '<a href="https://www.oyuna-erica.com/blog38">38</a>'  +
+    '<a href="https://www.oyuna-erica.com/blog37">37</a>'  +
+    '<a href="https://www.oyuna-erica.com/blog38">38</a>'  +
+    '<a href="https://www.oyuna-erica.com/blog39">39</a>'  +
 
         
     '<a href="#">&raquo;</a> ' +
